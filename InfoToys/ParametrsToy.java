40c9fcbebc8358a68abe582550250238e262ef73
Ksu <ksenia.barteva@gmail.com>
2023-06-15 12:16:36 +0700

    added package InfoToys

---
+++ InfoToys/ParametrsToy.java
@@ -0,0 +1,7 @@
+package InfoToys;
+public class ParametrsToy extends Toy {
+    public ParametrsToy(String name, int quantity, int weight) {
+        super(name, quantity, weight);
+        
+    }
+}
